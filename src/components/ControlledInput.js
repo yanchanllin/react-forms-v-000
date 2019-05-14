874a8d343f76204303e9bae2a980e508fcae7471
Yan C Lin <yonalin1@gmail.com>
2019-05-14 18:44:08 +0000

    Automatically backed up by Learn

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -6,19 +6,16 @@ class ControlledInput extends React.Component {
   }
 
   handleChange = event => {
-    this.setState({
-      value: event.target.value,
-    });
-  }
+  this.setState({
+    [event.target.name]: event.target.value
+  })
+}
 
   render() {
     return (
       <form onSubmit={event => this.handleSubmit(event)}>
-        <input
-          type="text"
-          value={this.state.value}
-          onChange={this.handleChange}
-        />
+      <input type="text" name="firstName" value={this.state.firstName} />
+      <input type="text" name="lastName" value={this.state.lastName} />
       </form>
     );
   }
