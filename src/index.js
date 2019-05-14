5b9194ca973bbeb4b4cf3da58e3794127be3d5be
Yan C Lin <yonalin1@gmail.com>
2019-05-14 18:42:26 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
- 
+
 import ControlledInput from './components/ControlledInput';
- 
+
 ReactDOM.render(
   <ControlledInput />,
   document.getElementById('root')
